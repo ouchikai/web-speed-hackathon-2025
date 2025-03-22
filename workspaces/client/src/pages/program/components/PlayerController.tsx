1de3f4b825baf32432c97788841fe85925587339
KaiOuchi <30932721+ouchikai@users.noreply.github.com>
2025-03-22 18:18:57 +0900

    fluentjson削除

--- workspaces/client/src/pages/program/components/PlayerController.tsx
+++ workspaces/client/src/pages/program/components/PlayerController.tsx
@@ -10,8 +10,15 @@ export const PlayerController = () => {
 
       <div className="absolute inset-x-0 bottom-0 px-[12px]">
         <div className="flex w-full flex-row items-center justify-between">
-          <div className="flex flex-row items-center">
-            <span className="i-fluent:live-24-filled m-[14px] block size-[20px] shrink-0 grow-0 text-[#FFFFFF]" />
+          <div className="flex items-center">
+            <svg
+              className="m-[14px] h-[20px] w-[20px] shrink-0 grow-0 text-[#FFFFFF]"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M6.343 4.938a1 1 0 0 1 0 1.415a8.003 8.003 0 0 0 0 11.317a1 1 0 1 1-1.414 1.414c-3.907-3.906-3.907-10.24 0-14.146a1 1 0 0 1 1.414 0m12.732 0c3.906 3.907 3.906 10.24 0 14.146a1 1 0 0 1-1.415-1.414a8.003 8.003 0 0 0 0-11.317a1 1 0 0 1 1.415-1.415M9.31 7.812a1 1 0 0 1 0 1.414a3.92 3.92 0 0 0 0 5.544a1 1 0 1 1-1.415 1.414a5.92 5.92 0 0 1 0-8.372a1 1 0 0 1 1.415 0m6.958 0a5.92 5.92 0 0 1 0 8.372a1 1 0 0 1-1.414-1.414a3.92 3.92 0 0 0 0-5.544a1 1 0 0 1 1.414-1.414m-4.186 2.77a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3" />
+            </svg>
             <span className="ml-[4px] block shrink-0 grow-0 text-[12px] font-bold text-[#FFFFFF]">ライブ配信</span>
           </div>
 

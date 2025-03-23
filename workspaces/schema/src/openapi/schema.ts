441884fedd1c0f9a4fdd2fef42cb23413b5a0692
KaiOuchi <30932721+ouchikai@users.noreply.github.com>
2025-03-23 11:34:47 +0900

    example文言削除

--- workspaces/schema/src/openapi/schema.ts
+++ workspaces/schema/src/openapi/schema.ts
@@ -20,7 +20,7 @@ const episode = z.object({
   title: z.string().openapi({ example: '第1話 吾輩は猫である' }),
   description: z.string().openapi({
     example:
-      '『吾輩は猫である』（わがはいはねこである）は、夏目漱石の長編小説であり、処女小説である。1905年（明治38年）1月、『ホトトギス』にて発表されたのだが、好評を博したため、翌1906年（明治39年）8月まで継続した。上、1906年10月刊、中、1906年11月刊、下、1907年5月刊。この文章は、クリエイティブ・コモンズ 表示-継承 4.0 国際 パブリック・ライセンスのもとで公表されたウィキペディアの項目「吾輩は猫である」（https://ja.wikipedia.org/wiki/吾輩は猫である）を素材として二次利用しています。',
+      '『吾輩は猫である』（わがはいはねこである）は、',
   }),
   order: z.number().openapi({ example: 1 }),
   seriesId: z.string().openapi({ format: 'uuid' }),
@@ -37,7 +37,7 @@ const series = z.object({
   title: z.string().openapi({ example: '吾輩は猫である' }),
   description: z.string().openapi({
     example:
-      '『吾輩は猫である』（わがはいはねこである）は、夏目漱石の長編小説であり、処女小説である。1905年（明治38年）1月、『ホトトギス』にて発表されたのだが、好評を博したため、翌1906年（明治39年）8月まで継続した。上、1906年10月刊、中、1906年11月刊、下、1907年5月刊。この文章は、クリエイティブ・コモンズ 表示-継承 4.0 国際 パブリック・ライセンスのもとで公表されたウィキペディアの項目「吾輩は猫である」（https://ja.wikipedia.org/wiki/吾輩は猫である）を素材として二次利用しています。',
+      '『吾輩は猫である』',
   }),
   thumbnailUrl: z.string().openapi({
     example: 'https://image.example.com/assets/d13d2e22-a7ff-44ba-94a3-5f025f2b63cd.png',
@@ -50,7 +50,7 @@ const program = z.object({
   title: z.string().openapi({ example: '吾輩は猫である' }),
   description: z.string().openapi({
     example:
-      '『吾輩は猫である』（わがはいはねこである）は、夏目漱石の長編小説であり、処女小説である。1905年（明治38年）1月、『ホトトギス』にて発表されたのだが、好評を博したため、翌1906年（明治39年）8月まで継続した。上、1906年10月刊、中、1906年11月刊、下、1907年5月刊。この文章は、クリエイティブ・コモンズ 表示-継承 4.0 国際 パブリック・ライセンスのもとで公表されたウィキペディアの項目「吾輩は猫である」（https://ja.wikipedia.org/wiki/吾輩は猫である）を素材として二次利用しています。',
+      '『吾輩は猫である』',
   }),
   startAt: z.string().openapi({ format: 'date-time' }),
   endAt: z.string().openapi({ format: 'date-time' }),

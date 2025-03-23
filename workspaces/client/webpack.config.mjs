34cd788c4435115aea11b7a9c18aadc4dbc2b3c0
KaiOuchi <30932721+ouchikai@users.noreply.github.com>
2025-03-23 17:16:24 +0900

    NODE_ENV production設定

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -92,7 +92,7 @@ const config = {
   },
 
   plugins: [
-    new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: '' }),
+    new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: 'production' }),
     // new BundleAnalyzerPlugin()
   ],
   resolve: {

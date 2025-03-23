0fcd3f28b9f34408197aae5ae8a050adea941889
KaiOuchi <30932721+ouchikai@users.noreply.github.com>
2025-03-23 15:30:15 +0900

    imgにサイズ指定（レイアウトシフト対策）

--- workspaces/client/src/features/recommended/components/SeriesItem.tsx
+++ workspaces/client/src/features/recommended/components/SeriesItem.tsx
@@ -21,7 +21,14 @@ export const SeriesItem = ({ series }: Props) => {
             <>
               <div className="relative overflow-hidden rounded-[8px] border-[2px] border-solid border-[#FFFFFF1F]">
                 <Flipped stagger flipId={isTransitioning ? `series-${series.id}` : 0}>
-                  <img alt="" className="h-auto w-full" loading="lazy" src={series.thumbnailUrl} />
+                  <img
+                    alt=""
+                    className="h-auto w-full"
+                    height={316}
+                    loading="lazy"
+                    src={series.thumbnailUrl}
+                    width={177.75}
+                  />
                 </Flipped>
               </div>
               <div className="p-[8px]">
